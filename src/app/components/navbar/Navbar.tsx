07a10ba90de7e8e421c1ac4b39c29075a2c80a28
midCenturyDude7 <mgriffes@gmail.com>
2024-01-16 19:25:39 -0800

    Setting up Navbar

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+import Styles from "./navbar.module.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
