6480aea2fa003a5c1bacb167a17e1212fa3fa0c2
midCenturyDude7 <mgriffes@gmail.com>
2024-01-18 20:59:19 -0800

    Implemented Framer-Motion

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -4,30 +4,75 @@ import Styles from "./navbar.module.scss";
 import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [burgerMenuActive, setBurgerMenuActive] = React.useState(false);
+
+  const toggleBurgerMenu = () => {
+    setBurgerMenuActive(!burgerMenuActive);
+  };
+
+  const motionVariants = {
+    open: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+        ease: "easeOut",
+        type: "spring",
+      },
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        staggerChildren: 0,
+        duration: 0,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.35,
+        ease: "easeOut",
+      },
+    },
+    closed: {
+      y: 100,
+      opacity: 0,
+      transition: {
+        duration: 0,
+      },
+    },
+  };
+
   return (
-    <div className={`${Styles.navbar} ${Styles.active}`}>
+    <div
+      className={`${Styles.navbar} ${burgerMenuActive ? Styles.active : ""}`}>
       <div className={Styles.navigation}>
         <button className={Styles.button}>learn More</button>
-        <div className={Styles.burgerMenuContainer}>
+        <div className={Styles.burgerMenuContainer} onClick={toggleBurgerMenu}>
           <div className={Styles.burgerMenuTrigger}></div>
           <div className={Styles.burgerMenu}></div>
         </div>
       </div>
       <div className={Styles.content}>
-        <ul>
-          <li>
+        <motion.ul
+          animate={burgerMenuActive ? "open" : "closed"}
+          variants={motionVariants}>
+          <motion.li variants={listItemVariants}>
             <a href="/">home</a>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
             <a href="/">about</a>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
             <a href="/">portfolio</a>
-          </li>
-          <li>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
             <a href="/">contact</a>
-          </li>
-        </ul>
+          </motion.li>
+        </motion.ul>
       </div>
     </div>
   );
